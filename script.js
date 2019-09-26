bde94e12f10bc485f9110edf36054631d4166a2b
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-09-26 17:58:38 +0300

    check

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-"use strict"
 
-
-function buttonClick() {
-    let cityName = document.getElementById("input").value;
-
+function buttonOnClickListener() {
+    let city = document.getElementById("input-field").value;
     let request = new XMLHttpRequest();
+    let requestURL = "https://api.openweathermap.org/data/2.5/weather?q="
+        + city +
+        "&appid=e972dcd233bab1ebce419c370711921f&units=metric&lang=en";
 
-    let requestText = "https://api.openweathermap.org/data/2.5/weather" +
-        "?q=" + cityName +
-        "&appid=7825ce4ffa896c5019e53087c858568a" +
-        "&units=metric" +
-        "&lang=en";
-    request.open("GET", requestText);
+    request.open("GET", requestURL);
     request.responseType = "json";
 
     request.onload = function () {
         if (request.status == 200) {
-            let cityName = request.response.name;
-            let imgHref = "http://openweathermap.org/img/wn/" + request.response.weather[0].icon + ".png";
-            updateTab("Weather in " + cityName, imgHref);
+            let city = request.response.name;
 
             let data = extractForecast(request.response);
             displayWeather(data);
@@ -39,24 +32,19 @@ function displayWeather(forecast) {
     document.getElementById("weather-container").innerHTML = html;
 }
 
-function updateTab(newTitle, newImage) {
-    document.title = newTitle;
-    document.getElementById("favicon").href = newImage;
-}
-
 function extractForecast(response) {
-    const iconSize = 64;
     let forecast =
     {
-        "city_name": response.name,
+        "city": response.name,
         "main": response.weather[0].description,
         "parameters":
             [
-                { "name": "Temperature", "value": response.main.temp, "units": "&deg;C", "icon" : "https://img.icons8.com/color/" + iconSize + "/000000/thermometer.png" },
-                { "name": "Pressure", "value": response.main.pressure, "units": "hPa", "icon" : "https://img.icons8.com/color/" + iconSize + "/000000/barometer-gauge.png"},
-                { "name": "Humidity", "value": response.main.humidity, "units": "%", "icon" : "https://img.icons8.com/color/" + iconSize + "/000000/hygrometer.png" },
-                { "name": "Clouds", "value": response.clouds.all, "units": "%", "icon" : "https://img.icons8.com/color/" + iconSize + "/000000/clouds.png" },
-                { "name": "Wind speed", "value": response.wind.speed, "units": "m/s", "icon" : "https://img.icons8.com/color/" + iconSize + "/000000/wind.png" },
+                { "type": "Temperature", "value": response.main.temp, "units": "&deg;C"},
+                { "type": "Pressure", "value": response.main.pressure, "units": "hPa"},
+                { "type": "Wind speed", "value": response.wind.speed, "units": "m/s"},
+                { "type": "Humidity", "value": response.main.humidity, "units": "%"},
+                { "type": "Clouds", "value": response.clouds.all, "units": "%"},
+                { "type": "Visibility", "value": response.visibility, "units": "m"},
             ],
     };
 
