0639588d8fb08aa654d05c6c9821e91f644cfd50
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-09-27 11:31:37 +0300

    end

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function myButtonOnClickListener() {
+function OnSubmitListener() {
     let city = document.getElementById("input-field").value;
     let request = new XMLHttpRequest();
     let requestURL = "https://api.openweathermap.org/data/2.5/weather?q="
